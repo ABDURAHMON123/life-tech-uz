3d0885bedde329e284f002c10ec2a525ae692712
ABDURAHMON123 <nazirjonovabdurahmon123@gmail.com>
2021-12-02 23:00:46 +0500

    Ustozlar blocks polniy bitdi

--- main.js
+++ main.js
@@ -16,3 +16,100 @@ document.querySelector('.yuborish').addEventListener('click', function() {
     let mobile = document.querySelector('.register');
     mobile.style.transform = 'translateX(-200%)'
 })
+
+document.querySelector('.ustozlar__knopkalar1').addEventListener('click', function() {
+    let ustozkotta = document.querySelector('.ustozlar__block__kotta');
+    let ustozknopka1 = document.querySelector('.ustozlar__knopkalar1');
+    let ustozknopka2 = document.querySelector('.ustozlar__knopkalar2');
+    let ustozknopka3 = document.querySelector('.ustozlar__knopkalar3');
+    let ustozknopka4 = document.querySelector('.ustozlar__knopkalar4');
+    let ustozknopka5 = document.querySelector('.ustozlar__knopkalar5');
+    let ustozknopka6 = document.querySelector('.ustozlar__knopkalar6');
+    ustozkotta.style.transform = 'translateY(0)'
+    ustozknopka1.style.background = 'greenyellow'
+    ustozknopka2.style.background = 'green'
+    ustozknopka3.style.background = 'green'
+    ustozknopka4.style.background = 'green'
+    ustozknopka5.style.background = 'green'
+    ustozknopka6.style.background = 'green'
+})
+document.querySelector('.ustozlar__knopkalar2').addEventListener('click', function() {
+    let ustozkotta = document.querySelector('.ustozlar__block__kotta');
+    let ustozknopka1 = document.querySelector('.ustozlar__knopkalar1');
+    let ustozknopka2 = document.querySelector('.ustozlar__knopkalar2');
+    let ustozknopka3 = document.querySelector('.ustozlar__knopkalar3');
+    let ustozknopka4 = document.querySelector('.ustozlar__knopkalar4');
+    let ustozknopka5 = document.querySelector('.ustozlar__knopkalar5');
+    let ustozknopka6 = document.querySelector('.ustozlar__knopkalar6');
+    ustozkotta.style.transform = 'translateY(-300px)'
+    ustozknopka1.style.background = 'green'
+    ustozknopka2.style.background = 'greenyellow'
+    ustozknopka3.style.background = 'green'
+    ustozknopka4.style.background = 'green'
+    ustozknopka5.style.background = 'green'
+    ustozknopka6.style.background = 'green'
+})
+document.querySelector('.ustozlar__knopkalar3').addEventListener('click', function() {
+    let ustozkotta = document.querySelector('.ustozlar__block__kotta');
+    let ustozknopka1 = document.querySelector('.ustozlar__knopkalar1');
+    let ustozknopka2 = document.querySelector('.ustozlar__knopkalar2');
+    let ustozknopka3 = document.querySelector('.ustozlar__knopkalar3');
+    let ustozknopka4 = document.querySelector('.ustozlar__knopkalar4');
+    let ustozknopka5 = document.querySelector('.ustozlar__knopkalar5');
+    let ustozknopka6 = document.querySelector('.ustozlar__knopkalar6');
+    ustozkotta.style.transform = 'translateY(-600px)'
+    ustozknopka1.style.background = 'green'
+    ustozknopka2.style.background = 'green'
+    ustozknopka3.style.background = 'greenyellow'
+    ustozknopka4.style.background = 'green'
+    ustozknopka5.style.background = 'green'
+    ustozknopka6.style.background = 'green'
+})
+document.querySelector('.ustozlar__knopkalar4').addEventListener('click', function() {
+    let ustozkotta = document.querySelector('.ustozlar__block__kotta');
+    let ustozknopka1 = document.querySelector('.ustozlar__knopkalar1');
+    let ustozknopka2 = document.querySelector('.ustozlar__knopkalar2');
+    let ustozknopka3 = document.querySelector('.ustozlar__knopkalar3');
+    let ustozknopka4 = document.querySelector('.ustozlar__knopkalar4');
+    let ustozknopka5 = document.querySelector('.ustozlar__knopkalar5');
+    let ustozknopka6 = document.querySelector('.ustozlar__knopkalar6');
+    ustozkotta.style.transform = 'translateY(-900px)'
+    ustozknopka1.style.background = 'green'
+    ustozknopka2.style.background = 'green'
+    ustozknopka3.style.background = 'green'
+    ustozknopka4.style.background = 'greenyellow'
+    ustozknopka5.style.background = 'green'
+    ustozknopka6.style.background = 'green'
+})
+document.querySelector('.ustozlar__knopkalar5').addEventListener('click', function() {
+    let ustozkotta = document.querySelector('.ustozlar__block__kotta');
+    let ustozknopka1 = document.querySelector('.ustozlar__knopkalar1');
+    let ustozknopka2 = document.querySelector('.ustozlar__knopkalar2');
+    let ustozknopka3 = document.querySelector('.ustozlar__knopkalar3');
+    let ustozknopka4 = document.querySelector('.ustozlar__knopkalar4');
+    let ustozknopka5 = document.querySelector('.ustozlar__knopkalar5');
+    let ustozknopka6 = document.querySelector('.ustozlar__knopkalar6');
+    ustozkotta.style.transform = 'translateY(-1200px)'
+    ustozknopka1.style.background = 'green'
+    ustozknopka2.style.background = 'green'
+    ustozknopka3.style.background = 'green'
+    ustozknopka4.style.background = 'green'
+    ustozknopka5.style.background = 'greenyellow'
+    ustozknopka6.style.background = 'green'
+})
+document.querySelector('.ustozlar__knopkalar6').addEventListener('click', function() {
+    let ustozkotta = document.querySelector('.ustozlar__block__kotta');
+    let ustozknopka1 = document.querySelector('.ustozlar__knopkalar1');
+    let ustozknopka2 = document.querySelector('.ustozlar__knopkalar2');
+    let ustozknopka3 = document.querySelector('.ustozlar__knopkalar3');
+    let ustozknopka4 = document.querySelector('.ustozlar__knopkalar4');
+    let ustozknopka5 = document.querySelector('.ustozlar__knopkalar5');
+    let ustozknopka6 = document.querySelector('.ustozlar__knopkalar6');
+    ustozkotta.style.transform = 'translateY(-1500px)'
+    ustozknopka1.style.background = 'green'
+    ustozknopka2.style.background = 'green'
+    ustozknopka3.style.background = 'green'
+    ustozknopka4.style.background = 'green'
+    ustozknopka5.style.background = 'green'
+    ustozknopka6.style.background = 'greenyellow'
+})
